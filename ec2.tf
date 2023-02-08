3b4214139e65b1c811b6473e48d34eca30418ffe
“WesJohnson-wrk <wesjohnson.wrk@gmail.com  git config --global user.email wesjohnson.wrk@gmail.com>
2023-02-08 17:50:55 -0500

    new branch

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,8 @@
-resource "aws_instance" "cloud" {
+resource "aws_instance" "node" {
   #count = 2
   ami                    = data.aws_ssm_parameter.instance_ami.value
   instance_type          = "t3.medium"
-  key_name               = "wjohnson"
-  subnet_id              = aws_subnet.public[0].id
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  tags = {
-    Name = "${var.default_tags.env}-EC2-cloud"
-  }
-  user_data = base64encode(file("./user_cloud.sh"))  # or us /Users/wesjohnson/Desktop/homebrew/q2_tf/user.sh
-}
-
-resource "aws_instance" "node" {
-  ami                    = data.aws_ssm_parameter.instance_ami.value
-  instance_type          = "t3.medium"
-  key_name               = "wjohnson"
+  key_name               = "wpj_USER"
   subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.instance.id]
 
@@ -23,6 +10,19 @@ resource "aws_instance" "node" {
     Name = "${var.default_tags.env}-EC2-node"
   }
   user_data = base64encode(file("./user_node.sh"))  # or us /Users/wesjohnson/Desktop/homebrew/q2_tf/user.sh
+}
+
+resource "aws_instance" "html" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  instance_type          = "t3.small"
+  key_name               = "wpj_USER"
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  tags = {
+    Name = "${var.default_tags.env}-EC2-html"
+  }
+  user_data = base64encode(file("./user_html.sh"))  # or us /Users/wesjohnson/Desktop/homebrew/q2_tf/user.sh
 }
 
 # output "ec2_ssh_command" {
